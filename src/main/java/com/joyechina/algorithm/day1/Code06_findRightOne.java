0771a571a7ca619498a5cc7745b0ee4435f90305
json-su <117433596@qq.com>
2022-02-23 15:59:01 +0800

    1
    异或练习

--- src/main/java/com/joyechina/algorithm/day1/Code06_findRightOne.java
+++ src/main/java/com/joyechina/algorithm/day1/Code06_findRightOne.java
@@ -7,21 +7,42 @@ package com.joyechina.algorithm.day1;
  **/
 public class Code06_findRightOne {
     public static int findRightOne(int num){
-        System.out.println("原始数字二进制形式");
+        //System.out.println("原始数字二进制形式");
         printNum(num);
-        System.out.println("提取最右侧的1之后二进制形式");
+      //  System.out.println("提取最右侧的1之后二进制形式");
         printNum(num &(-num));
         return num &(-num);
     }
 
-    public static void main(String[] args) {
-        System.out.println(findRightOne(124));
-    }
     public static  void printNum(int num){
         for(int i =31; i>= 0; i--){
             System.out.print(((num >> i) & 1));
         }
         System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        int i =12312321;
+        int j = 123453787;
+       printNum(i);
+       printNum(j);
+        System.out.println("i&j-------------------------");//
+        printNum(i&j);
+        System.out.println("end=======================");
+        System.out.println();
+        printNum(i);
+        printNum(j);
+        System.out.println("i|j-------------------------");
+        printNum(i|j);
+        System.out.println("end=======================");
+        System.out.println();
+        printNum(i);
+        printNum(j);
+        System.out.println("i^j-------------------------");
+        printNum(i^j);
+        System.out.println();
+       System.out.println(findRightOne(124));
     }
 
 }
--- src/main/java/com/joyechina/algorithm/day1/Code07_findTwoOddNum.java
+++ src/main/java/com/joyechina/algorithm/day1/Code07_findTwoOddNum.java
@@ -1,9 +1,32 @@
 package com.joyechina.algorithm.day1;
 
 /**
- * 给定一个数组 数组中元素 有两种数 出现奇数次 其余都是偶数次 找到该数
+ * 给定一个数组 数组中元素 有两种数出现奇数次 其余都是偶数次 找到该数
  * @author Administrator
  * @date 2020/11/18
  **/
 public class Code07_findTwoOddNum {
+
+    public static String findTwoOddNum(int[] array){
+        int eor = 0;
+        for(int i=0;i<array.length;i++){
+           eor ^= array[i];
+        }
+        //此时eor 的结果为 两个奇数次的数 异或 a^b
+        //lastOne 为找出a^b 最右侧的 1
+        int lastOne = eor & (-eor);
+        int onlyOneTimes = 0;
+        for(int i=0;i<array.length;i++){
+            //array[i]^ （a^b）最右侧的一 将原始数组分流 a与b必定可以分开
+            if((array[i] & lastOne)!=0){
+                onlyOneTimes ^=array[i];
+            }
+        }
+        System.out.println("one is :"+onlyOneTimes+"; another is: "+ (onlyOneTimes^eor));
+        return null;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,2,33,33,44,44,55,55};
+        findTwoOddNum(arr);
+    }
 }
